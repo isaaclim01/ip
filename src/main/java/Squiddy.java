6c88cfacf95a1804dd71fefa18b1eec74fb732b8
isaaclim01 <e1366771@u.nus.edu>
2025-08-21 01:17:34 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Squiddy.java
@@ -0,0 +1,9 @@
+public class Squiddy {
+    public static void main(String[] args) {
+        System.out.println("—".repeat(50));
+        System.out.println("Hello! I'm Squiddy \nWhat can I do for you?");
+        System.out.println("—".repeat(50));
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("—".repeat(50));
+    }
+}
