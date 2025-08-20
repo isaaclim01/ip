66bcd9d02b58a5949abd28cb98880910c51dcdbc
isaaclim01 <e1366771@u.nus.edu>
2025-08-21 02:22:09 +0800

    Mark as Done

--- src/main/java/Squiddy.java
+++ src/main/java/Squiddy.java
@@ -2,44 +2,77 @@ import java.util.Scanner;
 
 public class Squiddy {
 
-    public static String[] list = new String[100];
+    public static Task[] list = new Task[100];
     public static int counter = 0;
 
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
 
         System.out.println("—".repeat(50));
-        System.out.println("Hello! I'm Squiddy \nWhat can I do for you?");
+        System.out.println("I'm Squiddy, forced to be text in your terminal. " +
+                "\nType a task and I'll decide if I want to help you remember it");
         System.out.println("—".repeat(50));
 
-        String input = myScanner.nextLine();
+        String input = myScanner.nextLine().trim().toLowerCase();
         System.out.println("—".repeat(50));
 
         while (!input.equals("bye")) {
 
             if (input.equals("list")) {
+                System.out.println("Let's see what you've got: ");
+
                 for (int i = 0; i < counter; i++) {
-                    System.out.printf("%d. %s \n", i + 1, list[i]);
+                    Task curr = list[i];
+                    System.out.printf("%d.[%s] %s \n", i + 1,
+                            curr.getStatusIcon(),
+                            curr.getDescription());
                 }
 
-                System.out.println("—".repeat(50));
+            } else if (input.startsWith("mark ")) {
+                try {
+                    String numberStr = input.substring(5).trim();
+                    int number = Integer.parseInt(numberStr);
+                    Task curr = list[number - 1];
+                    curr.markDone();
 
-                input = myScanner.nextLine();
-                System.out.println("—".repeat(50));
+                    System.out.println("OK, you've completed this: ");
+                    System.out.printf("%d.[%s] %s \n", number,
+                            curr.getStatusIcon(),
+                            curr.getDescription());
+
+                } catch (NumberFormatException | StringIndexOutOfBoundsException e) {
+                    System.out.println("Don't start your task with the word 'mark'. Please.");
+                }
+
+            } else if (input.startsWith("unmark ")) {
+                try {
+                    String numberStr = input.substring(7).trim();
+                    int number = Integer.parseInt(numberStr);
+                    Task curr = list[number - 1];
+                    curr.unmarkDone();
+
+                    System.out.println("I hope this empty box make you feel bad for procrastinating: ");
+                    System.out.printf("%d.[%s] %s \n", number,
+                            curr.getStatusIcon(),
+                            curr.getDescription());
+
+                } catch (NumberFormatException | StringIndexOutOfBoundsException e) {
+                    System.out.println("Who uses unmark regularly? Use a more common word like 'erase'.");
+                }
 
             } else {
-                list[counter] = input;
+                list[counter] = new Task(input);
                 Squiddy.counter++;
 
-                System.out.println("added: " + input);
-                System.out.println("—".repeat(50));
-
-                input = myScanner.nextLine();
-                System.out.println("—".repeat(50));
+                System.out.println("Let me write this down: " + input);
             }
+            System.out.println("—".repeat(50));
+
+            input = myScanner.nextLine().trim().toLowerCase();
+            System.out.println("—".repeat(50));
         }
 
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Bye. Please don't bother me again.");
         System.out.println("—".repeat(50));
     }
 }
